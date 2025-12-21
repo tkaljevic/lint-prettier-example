61595702ce08c9745dc4e3a1d4c0cae5469dc310
Tomislav Kaljevic <tomislav.kaljevic@qposoft.com>
2025-12-21 14:35:42 +0100

    OPT-21 - OnPush rule added to existing eslint rules list

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	imports: [],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {
 	protected readonly title = signal('Eslint-Prettier-Husky');
