b85e232498ee06ccc3a4e64d90023a2a31c8c13c
Tomislav Kaljevic <tomislav.kaljevic@qposoft.com>
2025-12-21 13:40:15 +0100

    OPT-17 - Readme updated

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet],
+	imports: [],
 	templateUrl: './app.html',
 	styleUrl: './app.scss',
 })
 export class App {
-	protected readonly title = signal('optimal');
+	protected readonly title = signal('Eslint-Prettier-Husky');
 }
